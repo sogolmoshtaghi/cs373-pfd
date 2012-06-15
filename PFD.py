ad05724b2579fa7b9dacfb93196638d52a5bd0ec
smoshtaghi <sogol@utexas.edu>
2012-06-15 18:18:25 -0500

    made a list of successors and added a unit test

--- PFD.py
+++ PFD.py
@@ -24,36 +24,36 @@ return true if that succeeds, false otherwise
     l = s.split()
     size = len(a[0])
     l_idx = 1
-    print l
     while l_idx < size :
 		assert int(l[l_idx]) > 0 
 		assert int(l[l_idx]) < 101
 		assert int(l[0]) != 0 
-		print a
 		a[int(l[0])][l_idx-1] = int(l[l_idx])
-		print a
 		l_idx += 1
 		#size -= 1
     return True
 
 
 
-def PFD_eval(i):
-	return 1
+def PFD_eval(i,j):
+	i_size = len(i[0])
+	idx = 1
+	while i_size >  idx :
+		jdx = 1
+		while i_size > jdx :
+			if i[idx][jdx] != None :
+				j[i[idx][jdx]].append(idx)
+			jdx += 1
+		idx += 1
+	return j
+
+	
+	
+	
 
 
 def PFD_print(w,v):
-#	size2 = len(arrayprint[0])
-#	size3 = len(arrayprint)
-#	idxtwo = 0
-#	idx3 = 0
-#	while size3 > 0 :
-#		while size2 > 0 :
-#			w.write(str(arrayprint[idxtwo][idx3])
-#			idxtwo += 1
-#			size2 -= 1
-#		idx3 += 1
-#		size3 -= 1
+
 	w.write("high")	
 	
 
@@ -63,15 +63,15 @@ read, eval, print loop
 r is a reader
 w is a writer
 """
-    s = r.readline()
-    if s == "" :
-		return false
-    l = s.split()
-    vertex_count = int(l[0])
-    rule_count = int(l[1])
-    idx = 0
-    #a = [[0] * (vertex_count+1)] * (vertex_count+1)
-    a = [[None]*(vertex_count+1) for _ in range(vertex_count+1)]
-    while PFD_read(r, a) :
-        v = PFD_eval(a)
-        PFD_print(w,v)
+    s = r.readline()												
+    if s == "" :													
+		return false												
+    l = s.split()													
+    vertex_count = int(l[0])										
+    rule_count = int(l[1])																														
+    a = [[None]*(vertex_count+1) for _ in range(vertex_count+1)]	#Creates 2D list that has individual references to each cell
+    successors = [[None]*(1) for _ in range(vertex_count+1)]
+    print successors
+    while PFD_read(r, a) :											#Reads individual lines, with the first one already read in line #66
+        v = PFD_eval(a, successors)												
+        PFD_print(w,v)												
--- TestPFD.py
+++ TestPFD.py
@@ -35,8 +35,20 @@ class TestPFD (unittest.TestCase) :
         a = [[None]*(3) for _ in range(3)]
         b = PFD_read(r, a)
         self.assert_(b == True)
-        print a
+        #print a
         #self.assert_(a[2][0] == 2)
+        
+	# ----
+    # eval
+    # ----
+
+    def test_eval(self) :
+		a = [[None, None, None, None, None, None], [None, None, None, None, None, None], [2, 5, 3, None, None, None], [2, 1, 5, None, None, None], [1, 3, None, None, None, None], [1, 1, None, None, None, None]]
+		b = [[None]*(1) for _ in range(6)]
+		v = PFD_eval(a, b)
+		print v
+
+
         
         
         
