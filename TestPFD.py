7ca18a400ee2ae08a4437e17b277370a24f6a890
smoshtaghi <sogol@utexas.edu>
2012-06-17 17:14:01 -0500

    Added some more unit tests and Fixed issue #12

--- TestPFD.py
+++ TestPFD.py
@@ -18,8 +18,9 @@ To test the program:
 
 import StringIO
 import unittest
+import sys
 
-from PFD import PFD_read, PFD_eval, PFD_print, PFD_solve
+from PFD import PFD_read, PFD_eval, PFD_print, PFD_solve, PFD_removal
 
 # -----------
 # TestPFD
@@ -30,32 +31,83 @@ class TestPFD (unittest.TestCase) :
     # read
     # ----
 
-    def test_read (self) :
-        r = StringIO.StringIO("1 1 2\n")
+    def test_read1 (self) :
+        r = StringIO.StringIO("2 1 1\n1 1 2")
         a = [[0]*(3) for _ in range(3)]
+        successors = [[0]*(1) for _ in range(3)]
         b = PFD_read(r, a)
-        #self.assert_(b == True)
-        #self.assert_(a[2][0] == 2)
+        self.assert_(b == True)
+        self.assert_(a[0][0] == 0)
+        self.assert_(a[0][1] == 0)
+        self.assert_(a[0][2] == 0)
+        b = PFD_read(r, a)
+        self.assert_(a[1][0] == 1)
+        self.assert_(a[1][1] == 2)
+        self.assert_(a[1][2] == 0)  
+        b = PFD_read(r, a)      
+        self.assert_(a[2][0] == 1)
+        self.assert_(a[2][1] == 1)
+        self.assert_(a[2][2] == 0)
+        
+        
+    def test_read2 (self) :
+        r = StringIO.StringIO("3 2 1 5")
+        a = [[0]*(5) for _ in range(5)]
+        b = PFD_read(r, a)
+        self.assert_(b == True)
+        self.assert_(a[0][0] == 0)
+        self.assert_(a[0][1] == 0)
+        self.assert_(a[0][2] == 0)
+        self.assert_(a[3][0] == 2)
+        self.assert_(a[3][1] == 1)
+        self.assert_(a[3][2] == 5)
+        
+    def test_read3 (self) :
+        r = StringIO.StringIO("1 7 3 4 5 6 7 8 9")
+        a = [[0]*(9) for _ in range(9)]
+        b = PFD_read(r, a)
+        self.assert_(b == True)
+        self.assert_(a[1][0] == 7)
+        self.assert_(a[1][1] == 3)
+        self.assert_(a[1][2] == 4)
+        self.assert_(a[1][3] == 5)
+        self.assert_(a[1][4] == 6)
+        self.assert_(a[1][5] == 7)    
+        self.assert_(a[1][6] == 8)
+        self.assert_(a[1][7] == 9)        
         
 	# ----
     # eval
     # ----
+    def test_eval1 (self):
+		a = [[0,0,0],[1,2,0],[1,1,0]]
+		successors = [[0]*(1) for _ in range(3)]
+		w = sys.stdout
+		b = PFD_eval(a,successors,w)
+		self.assert_(successors[1][1] == 2)        #Testing the list of successors
+		self.assert_(successors[2][1] == 1)
+		self.assert_(successors[0][0] == 0)
+		self.assert_(successors[1][0] == 0)
 
-    def test_eval(self) :
+    def test_eval2 (self):
 		a = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [2, 5, 3, 0, 0, 0], [2, 1, 5, 0, 0, 0], [1, 3, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0]]
-		b = [[0]*(1) for _ in range(6)]
-		v = PFD_eval(a, b)
-		print v
-
-    def test_PFD_removal(self) :
-		a = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [2, 5, 3, 0, 0, 0], [2, 1, 5, 0, 0, 0], [1, 3, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0]]
-		b = [[0]*(1) for _ in range(6)]
-		v = PFD_eval(a, b)
-		print "result= ",v
-        
-        
-        
-        
+		successors = [[0]*(1) for _ in range(6)]
+		w = sys.stdout
+		b = PFD_eval(a,successors,w)
+		self.assert_(successors == [[0], [0,3,5], [0], [0, 2,4], [0], [0,2,3]])        #Testing the list of successors
+		self.assert_(successors[1][1] == 3)
+		self.assert_(successors[1][2] == 5)
+		self.assert_(successors[1][0] == 0)     
+   
+    def test_eval3 (self):
+		a = [[0,0,0],[1,2,0],[1,1,0]]
+		successors = [[0]*(1) for _ in range(3)]
+		w = sys.stdout
+		b = PFD_eval(a,successors,w)
+		self.assert_(successors[1][1] == 2)        #Testing the list of successors
+		self.assert_(successors[2][1] == 1)
+		self.assert_(successors[0][0] == 0)
+		self.assert_(successors[1][0] == 0)       
         
 # ----
 # main
